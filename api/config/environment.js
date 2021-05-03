175cbd8c81089f4265440fad10e6570b1b3b523d
MadNelis <njhoek@avans.nl>
2021-05-03 18:07:48 +0200

    Week 3 (No live editing)

--- api/config/environment.js
+++ api/config/environment.js
@@ -3,15 +3,13 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
-const {
-  username,
-  password,
-  host,
-  port,
-  database,
-  params,
-  secret,
-} = process.env;
+const username = process.env.username || '';
+const password = process.env.password || '';
+const host = process.env.DB || 'localhost';
+const port = process.env.port || '27017';
+const database = process.env.database || 'Connections';
+const params = process.env.params || '';
+const secret = process.env.secret || 'MyMassiveSecret';
 
 let uri = 'mongodb://';
 if (username && password) {
